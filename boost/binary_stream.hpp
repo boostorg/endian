793e21594c36e925253bd65bd6f9f531b3b3a258
bemandawes <bemandawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-04-22 20:34:30 +0000

    A fresh approach for binary_stream.hpp
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/endian@71421 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/binary_stream.hpp
+++ boost/binary_stream.hpp
@@ -1,6 +1,6 @@
 //  boost/binary_stream.hpp  ----------------------------------------------------------//
 
-//  Copyright Beman Dawes 2009
+//  Copyright Beman Dawes 2009, 2011
 
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
@@ -20,7 +20,7 @@
 # include <cwchar>   // for wcslen
 #endif
 
-//  unformatted binary (as opposed to formatted character-set) input and output
+//  unformatted binary (as opposed to formatted character) input and output
 
 //  Caution: Use only on streams opened with filemode std::ios_base::binary. Thus
 //  unformatted binary I/O should not be with the standard streams (cout, cin, etc.)
@@ -28,127 +28,54 @@
 //  results, such as insertion of unwanted characters or premature end-of-file.
 //  For example, on Windows 0x0D would become 0x0D, 0x0A.
 
-//  Caution: When mixing formatted (i.e. operator << or >>) and unformatted (i.e.
-//  operator <= or >=) be aware that << and >> take precedence over <= and >=. Use
-//  parentheses to force correct order of evaluation. For example:
-//
-//          my_stream << foo <= bar;    // no parentheses needed
-//          (my_stream <= foo) << bar;  // parentheses required 
-
-//  This implementation uses reinterpret_cast<>() when needed to convert one pointer
-//  type to another. See 5.2.10 [expr.reinterpret.cast], Reinterpret cast, para 7.
-
 namespace boost
 {
 
-  //  built-in types  ------------------------------------------------------------------//
+namespace detail
+{
+  template <class T>
+  struct const_binary_data
+  {
+    const char* ptr;
+    explicit const_binary_data(const T& x) : ptr(reinterpret_cast<const char*>(&x)) {}
+  };
+  template <class T>
+  struct binary_data
+  {
+    char* ptr;
+    explicit binary_data(T& x) : ptr(reinterpret_cast<char*>(&x)) {}
+  };
+}
 
-  //  omission of bool and void* is deliberate; any semantics would be questionable
+template <class T>
+inline detail::const_binary_data<T> bin(const T& x)
+{
+  return detail::const_binary_data<T>(x);
+}
 
-  inline std::ostream& operator<=(std::ostream& os, short v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, short& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
+template <class T>
+inline detail::binary_data<T> bin(T& x)
+{
+  return detail::binary_data<T>(x);
+}
 
-  inline std::ostream& operator<=(std::ostream& os, unsigned short v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, unsigned short& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
+template <class T>
+inline std::ostream& operator<<(std::ostream& os, detail::const_binary_data<T> x)
+{
+  return os.write(x.ptr, sizeof(T));
+}
 
-  inline std::ostream& operator<=(std::ostream& os, int v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, int& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
+template <class T>
+inline std::ostream& operator<<(std::ostream& os, detail::binary_data<T> x)
+{
+  return os.write(x.ptr, sizeof(T));
+}
 
-  inline std::ostream& operator<=(std::ostream& os, unsigned int v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, unsigned int& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, long v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, long& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, unsigned long v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, unsigned long& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, long long v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, long long& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, unsigned long long v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, unsigned long long& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, float v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, float& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, double v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, double& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, long double v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, long double& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, char c)
-    { return os.put( c ); }
-  inline std::istream& operator>=(std::istream& is, char& c)
-    { return is.get( c ); }
-
-  inline std::ostream& operator<=(std::ostream& os, signed char c)
-    { return os.put( c ); }
-  inline std::istream& operator>=(std::istream& is, signed char& c)
-    { return is.get( reinterpret_cast<char&>(c) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, unsigned char c)
-    { return os.put( c ); }
-  inline std::istream& operator>=(std::istream& is, unsigned char& c)
-    { return is.get( reinterpret_cast<char&>(c) ); }
-
-  inline std::ostream& operator<=(std::ostream& os, wchar_t v)
-    { return os.write( reinterpret_cast<const char*>(&v), sizeof(v) ); }
-  inline std::istream& operator>=(std::istream& is, wchar_t& v)
-    { return is.read( reinterpret_cast<char*>(&v), sizeof(v) ); }
-
-  //  strings  -------------------------------------------------------------------------//
-
-  inline std::ostream& operator<=(std::ostream& os, const char* p)
-    { return os.write( p, std::strlen(p)+1 ); }
-
-  inline std::ostream& operator<=(std::ostream& os, const signed char* p)
-    { return os.write( reinterpret_cast<const char*>(p), std::strlen(reinterpret_cast<const char*>(p))+1 ); }
-
-  inline std::ostream& operator<=(std::ostream& os, const unsigned char* p)
-    { return os.write( reinterpret_cast<const char*>(p), std::strlen(reinterpret_cast<const char*>(p))+1 ); }
-
-#ifndef BOOST_NO_CWCHAR
-  inline std::ostream& operator<=(std::ostream& os, const wchar_t* p)
-    { return os.write( reinterpret_cast<const char*>(p), (std::wcslen(p)+1)*sizeof(wchar_t) ); }
-#endif
-
-  //  Caution: note the asymmetry between output and input; a string with embedded
-  //  nulls will be output with the embedded nulls, but input will stop at the first null.
-  //  So it probably isn't a good idea to use these functions for strings with nulls.
-  inline std::ostream& operator<=(std::ostream& os, const std::string& s)
-    { return os.write( s.c_str(), s.size()+1 ); }
-  inline std::istream& operator>=(std::istream& is, std::string& s)
-    { return getline(is, s, '\0'); }
-
-#ifndef BOOST_NO_STD_WSTRING
-  inline std::ostream& operator<=(std::ostream& os, const std::wstring& s)
-    { return os.write( reinterpret_cast<const char*>(s.c_str()), (s.size()+1)*sizeof(wchar_t) ); }
-  // TODO: provide input function
-#endif
+template <class T>
+inline std::istream& operator>>(std::istream& is, detail::binary_data<T> x)
+{
+  return is.read(x.ptr, sizeof(T));
+}
 
 }  // namespace boost
 
