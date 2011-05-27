5e802bea7f1374ffd6ba8774ca80178b704b7a56
bemandawes <bemandawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-05-27 15:23:23 +0000

    Rename boost/endian/integers.hpp to boost/endian/types.hpp for clarity and in case some non-integer types get added in the future. Fix include in bin_manip_example.cpp
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/endian@72212 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/io/example/bin_manip_example.cpp
+++ libs/io/example/bin_manip_example.cpp
@@ -5,7 +5,7 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/binary_stream.hpp>
+#include <boost/io/detail/bin_manip.hpp>
 #include <boost/integer.hpp>
 #include <boost/assert.hpp>
 #include <fstream>
