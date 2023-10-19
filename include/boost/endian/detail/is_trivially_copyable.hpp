90a8527bbfee167e8b00f9518b548c0487ec942e
Peter Dimov <pdimov@gmail.com>
2023-10-19 20:05:06 +0300

    Fix is_trivially_copyable again

--- include/boost/endian/detail/is_trivially_copyable.hpp
+++ include/boost/endian/detail/is_trivially_copyable.hpp
@@ -1,8 +1,7 @@
 #ifndef BOOST_ENDIAN_DETAIL_IS_TRIVIALLY_COPYABLE_HPP_INCLUDED
 #define BOOST_ENDIAN_DETAIL_IS_TRIVIALLY_COPYABLE_HPP_INCLUDED
 
-// Copyright 2019 Peter Dimov
-//
+// Copyright 2019, 2023 Peter Dimov
 // Distributed under the Boost Software License, Version 1.0.
 // http://www.boost.org/LICENSE_1_0.txt
 
@@ -19,7 +18,7 @@ namespace detail
 #if defined( BOOST_LIBSTDCXX_VERSION ) && BOOST_LIBSTDCXX_VERSION < 50000
 
 template<class T> struct is_trivially_copyable: std::integral_constant<bool,
-    std::has_trivial_copy_constructor<T>::value && std::has_trivial_copy_assign<T>::value && std::has_trivial_destructor<T>::value> {};
+    __has_trivial_copy(T) && __has_trivial_assign(T) && __has_trivial_destructor(T)> {};
 
 #else
 
